91e095d0e62e4ce402a5d758cb6e8ee7ced72fa8
Kate Liu <katewjliu@gmail.com>
2025-05-01 21:08:36 +0000

    Fixed get spectrum method to properly extract centroid data

--- Raw_data_converter/Program.cs
+++ Raw_data_converter/Program.cs
@@ -94,9 +94,7 @@ namespace RawFileConverter
                 if (writeSpectrum)
                 {
                     // Create a binary file based on raw file name
-                    //string binFileName = Path.ChangeExtension(filename, ".bin");
-                   
-                    string binFileName = "test_binary.bin";
+                    string binFileName = Path.ChangeExtension(filename, ".bin");
 
                     GetAllSpectra(rawFile, binFileName);
                     Console.WriteLine("Spectrum data stored in binary file: " + binFileName + " (" + binFileName.Length + " bytes)");
@@ -126,129 +124,63 @@ namespace RawFileConverter
                 int lastScan = rawFile.RunHeaderEx.LastSpectrum;
                 double startTime = rawFile.RunHeaderEx.StartTime;
                 double endTime = rawFile.RunHeaderEx.EndTime;
-                //writer.Write(firstScan);
-                //writer.Write(lastScan);
-                //writer.Write(startTime);
-                //writer.Write(endTime);
 
                 // Write the total number of scans.
                 int numScans = lastScan - firstScan + 1;
-                //writer.Write(numScans);
+                writer.Write(numScans);
 
                 // Loop over every scan.
                 for (int scanNumber = firstScan; scanNumber <= lastScan; scanNumber++)
                 {
-                    Console.WriteLine("get all spectrum");
-                    //CentroidScan(rawFile, scanNumber);
-                    GetSpectrum(rawFile, scanNumber, writer);
+                    try
+                    {
+                        // write scan # in binary file 
+                        writer.Write(scanNumber);
+
+                        // Get the scan filter for the spectrum
+                        var scanFilter = rawFile.GetFilterForScanNumber(scanNumber);
+
+                        if (string.IsNullOrEmpty(scanFilter.ToString()))
+                        {
+                            continue;
+                        }
+
+                        var scan = Scan.FromFile(rawFile, scanNumber);
+                        
+                        // If that scan contains FTMS data then Centroid stream will be populated so check to see if it is present.
+                        int labelSize = 0;
+
+                        if (scan.HasCentroidStream)
+                        {
+                            
+                            labelSize = scan.CentroidScan.Length;
+                            writer.Write(labelSize);
+                        }
+
+                        for (int i=0; i<labelSize; i++)
+                        {
+                            //Console.WriteLine("Spectrum " + i + ": "+ scan.CentroidScan.Masses[i]+ ", "+ scan.CentroidScan.Intensities[i]);
+                            // write to bin
+                            writer.Write(scan.CentroidScan.Masses[i]);      // double
+                            writer.Write(scan.CentroidScan.Intensities[i]);   // double
+                           
+
+                        }
+
+                        // For non-FTMS data, the preferred data will be populated
+                        int dataSize = scan.PreferredMasses.Length;
+                        Console.WriteLine("Spectrum {0} - {1}: normal {2}, label {3} points", scanNumber, scanFilter.ToString(), dataSize, labelSize);
+                        
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Error reading spectrum {0} - {1}", scanNumber, ex.Message);
+                    }
                     
                 }
             }
         }
 
-        // Method to centroid a scan
-        private static void CentroidScan(IRawDataPlus rawFile, int scanNumber)
-        {
-            // Get the scan from the RAW file
-            var scan = Scan.FromFile(rawFile, scanNumber);
-
-            // Centroid the scan
-            var centroidedScan = Scan.ToCentroid(scan);
-
-            Console.WriteLine("test centroid for scan # {}", scanNumber);
-            /*
-            // Reflection to find object's properties and methods
-            Type type = centroidedScan.GetType();
-            // List properties
-            Console.WriteLine("Properties:");
-            foreach (PropertyInfo prop in type.GetProperties())
-                Console.WriteLine(prop.Name);
-            Console.WriteLine("Methods:");
-            // List methods
-            foreach (MethodInfo method in type.GetMethods())
-                Console.WriteLine(method.Name);
-            
-            var masses = centroidedScan.PreferredMasses;
-            var intensities = centroidedScan.PreferredIntensities;
-
-            for (int i=0; i< masses.Length; i++)
-            {
-                Console.WriteLine("  {0:F4} {1:F0}", masses[i], intensities[i]);
-            }
-            */
-
-            // See if the returned scan object has a centroid stream
-            if (centroidedScan.HasCentroidStream)
-            {
-                Console.WriteLine("returned scan has centroid stream"); //never executed
-                Console.WriteLine("Average spectrum ({0} points)", centroidedScan.CentroidScan.Length);
-
-                // Print the spectral data (mass, intensity values)
-                for (int i = 0; i < centroidedScan.CentroidScan.Length; i++)
-                {
-                    Console.WriteLine("  {0:F4} {1:F0}", centroidedScan.CentroidScan.Masses[i], centroidedScan.CentroidScan.Intensities[i]);
-                }
-            
-            }
-        }
-
-        // Method to retrieve spectrum data for a single scan
-        // we first write the scan number and then a “data type” flag (1 for centroid scans, 0 for profile scans)
-        private static void GetSpectrum(IRawDataPlus rawFile, int scanNumber, BinaryWriter writer)
-        {
-            // Get the scan statistics from the RAW file for this scan number
-            var scanStatistics = rawFile.GetScanStatsForScanNumber(scanNumber);
-
-            // Write the scan number.
-            writer.Write(scanNumber);
-
-            // Check to see if the scan has centroid data or profile data.
-
-            if (scanStatistics.IsCentroidScan && scanStatistics.SpectrumPacketType == SpectrumPacketType.FtCentroid)
-            {
-                writer.Write((byte)1);
-
-                // Get the centroid (label) data from the RAW file for this scan
-                var centroidStream = rawFile.GetCentroidStream(scanNumber, false);
-                int numPoints = centroidStream.Length;
-                writer.Write(numPoints);
-                Console.WriteLine("Spectrum (centroid/label) {0} - {1} points", scanNumber, centroidStream.Length);
-
-                // Print the spectral data (mass, intensity, charge values).  Not all of the information in the high resolution centroid 
-                // (label data) object is reported in this example.  Please check the documentation for more information about what is
-                // available in high resolution centroid (label) data.
-                
-                for (int i = 0; i < numPoints; i++)
-                {
-                    writer.Write(centroidStream.Masses[i]);      // double
-                    writer.Write(centroidStream.Intensities[i]);   // double
-                    writer.Write(centroidStream.Charges[i]);       // int
-                    //Console.WriteLine("  {0} - {1:F4}, {2:F0}, {3:F0}", i, centroidStream.Masses[i], centroidStream.Intensities[i], centroidStream.Charges[i]);
-                }
-                
-            }
-            else
-            {
-                writer.Write((byte)0);
-
-                // Get the segmented (low res and profile) scan data
-                
-                var segmentedScan = rawFile.GetSegmentedScanFromScanNumber(scanNumber, scanStatistics);
-                int numPoints = segmentedScan.Positions.Length;
-                writer.Write(numPoints);
-                Console.WriteLine("Spectrum (profile data) {0} - {1} points", scanNumber, segmentedScan.Positions.Length);
-
-                // Print the spectral data (mass, intensity values) 
-                for (int i = 0; i < numPoints; i++)
-                {
-                    writer.Write(segmentedScan.Positions[i]);    // double
-                    writer.Write(segmentedScan.Intensities[i]);    // double
-                    if (i < 10){
-                    Console.WriteLine("  {0} - {1:F4}, {2:F0}", i, segmentedScan.Positions[i], segmentedScan.Intensities[i]);
-                    }
-                }
-                
-            }
-        }
+        
     }
 }
